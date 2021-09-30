83a3f6b9b95857141a50138a5b9464736fae2754
catsbi <killfine1988@gmail.com>
2021-09-30 21:39:13 +0900

    feat: Ball 일급컬렉션 BallList및 BallListTest 생성

---
+++ src/main/java/baseball/domain/BallList.java
@@ -0,0 +1,11 @@
+package baseball.domain;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 야구 게임 숫자 일급 컬렉션
+ */
+public class BallList {
+    private final List<Ball> store = new ArrayList<>();
+}
---
+++ src/test/java/baseball/domain/BallListTest.java
@@ -0,0 +1,10 @@
+package baseball.domain;
+
+import org.junit.jupiter.api.DisplayName;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@DisplayName("BallList 클래스")
+class BallListTest {
+
+}
