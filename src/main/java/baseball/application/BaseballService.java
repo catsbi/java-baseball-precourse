b4bf346cbe57e818a230e4e41ff6db519da9a252
catsbi <killfine198@gmail.com>
2021-10-06 11:45:49 +0900

    feat: BaseballService 테스트 케이스 작성 및 로직 구현

--- src/main/java/baseball/application/BaseballService.java
+++ src/main/java/baseball/application/BaseballService.java
@@ -1,8 +1,40 @@
 package baseball.application;
 
+import baseball.domain.BallList;
+import baseball.domain.BaseballGame;
+import baseball.domain.BaseballGameRecords;
+import baseball.domain.DomainGenerator;
+
 /**
  * 야구 게임에 대한 서비스를 제공한다.
  */
 public class BaseballService {
+    private final DomainGenerator<BallList> ballListGenerator;
+
+    public BaseballService(DomainGenerator<BallList> ballListGenerator) {
+        this.ballListGenerator = ballListGenerator;
+    }
+
+    /**
+     * 새로운 숫자 야구 게임 도메인을 만들어 반환한다.
+     *
+     * @return 야구 게임
+     */
+    public BaseballGame createGame() {
+        return BaseballGame.newInstance(ballListGenerator);
+    }
+
+    /**
+     * 숫자 야구 게임을 진행하고 진행 결과를 반환한다.
+     *
+     * @param baseballGame 숫자 야구 게임
+     * @param numStrings 사용자가 입력한 숫자 문자열
+     * @return 숫자 야구 게임 결과
+     */
+    public BaseballGameRecords play(BaseballGame baseballGame, String numStrings) {
+        final BallList ballList = BallList.from(numStrings);
+
+        return baseballGame.matches(ballList);
+    }
 
 }
--- src/test/java/baseball/application/BaseBallServiceTest.java
+++ src/test/java/baseball/application/BaseBallServiceTest.java
@@ -1,7 +1,27 @@
 package baseball.application;
 
+import baseball.domain.BallList;
+import baseball.domain.BaseballGame;
+import baseball.domain.BaseballGameRecords;
+import baseball.domain.DomainGenerator;
+import baseball.domain.RandomBallListGenerator;
+import baseball.domain.Zone;
+import baseball.exception.DuplicateBallException;
+import baseball.exception.InvalidBallListSizeException;
+import baseball.exception.InvalidBallValueException;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.mock;
 
 @DisplayName("BaseBallService 클래스")
 class BaseBallServiceTest {
@@ -9,7 +29,68 @@ class BaseBallServiceTest {
 
     @BeforeEach
     void setUp() {
-        baseBallService = new BaseballService();
+        DomainGenerator<BallList> generator = mock(RandomBallListGenerator.class);
+        baseBallService = new BaseballService(generator);
+
+
+        given(generator.generate()).willReturn(BallList.from("123"));
+    }
+
+    @DisplayName("createGame 메서드는, 새로운 야구 게임 객체를 만들어 반환한다.")
+    @Test
+    void createGame() {
+        final BaseballGame game = baseBallService.createGame();
+        final BaseballGameRecords records = game.matches(BallList.from("123"));
+
+        assertThat(game).isInstanceOf(BaseballGame.class);
+        assertThat(records.countByZone(Zone.STRIKE)).isEqualTo(3);
+    }
+
+    @DisplayName("play 메서드는 ")
+    @Nested
+    class Describe_play {
+        private BaseballGame baseballGame;
+
+        @BeforeEach
+        void setUp() {
+            baseballGame = baseBallService.createGame();
+        }
+
+        @DisplayName("인자가 유효 할 경우, 인자 값을 비교해 비교 결과를 반환한다.")
+        @ParameterizedTest
+        @CsvSource(value = {"123:3", "142:1", "153:2", "921:1", "723:2"}, delimiter = ':')
+        void playWithValidData(String numStrings, int strikeCount) {
+            final BaseballGameRecords records = baseBallService.play(baseballGame, numStrings);
+
+            assertThat(records.countByZone(Zone.STRIKE)).isEqualTo(strikeCount);
+
+        }
+
+        @DisplayName("사용자의 입력 값이 3자리가 아닌 경우, 예외를 던진다. ")
+        @ParameterizedTest
+        @NullAndEmptySource
+        @ValueSource(strings = {"1234", "5678", "12", "1", "999999999999"})
+        void playWithInvalidLengthNumbStrings(String numStrings) {
+
+            assertThatThrownBy(() -> baseBallService.play(baseballGame, numStrings))
+                    .isInstanceOf(InvalidBallListSizeException.class);
+        }
+
+        @DisplayName("사용자의 입력 문자열의 각각의 문자가 숫자가 아닌 경우, 예외를 던진다.")
+        @ParameterizedTest
+        @ValueSource(strings = {"12A", "abc", "9c5", "aaa", "3gh", "5d1", "93d"})
+        void playWithInvalidTypeNumStrings(String numStrings) {
+            assertThatThrownBy(() -> baseBallService.play(baseballGame, numStrings))
+                    .isInstanceOf(InvalidBallValueException.class);
+        }
+
+        @DisplayName("사용자의 입력 문자열에 중복된 숫자가 있을 경우, 예외를 던진다.")
+        @ParameterizedTest
+        @ValueSource(strings = {"151", "244", "336", "811", "999", "111", "272"})
+        void playWithDuplicatedNumStrings(String numStrings) {
+            assertThatThrownBy(()-> baseBallService.play(baseballGame, numStrings))
+                    .isInstanceOf(DuplicateBallException.class);
+        }
     }
 
 }
